5a450a481e32a33e2a211feb88236efdf9c59def
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-07-28 02:10:21 +0530

    Update Recursive Quick sort.cpp

--- Week05/Recursive Quick sort.cpp
+++ Week05/Recursive Quick sort.cpp
@@ -25,7 +25,8 @@ int partitions(int arr[],int start,int end){
     return i;
 }
 void quicksort(int arr[],int start,int end){
-    if(end<=start){
+    if(end<=start)
+    {
         return;  //base
     }
     int pivot;
@@ -34,28 +35,28 @@ void quicksort(int arr[],int start,int end){
     quicksort(arr,pivot+1,end);
 
 }
-int main()
-
-{   int sizze;
-
-
-        for(int j=1000;j<=10000;j+=2000){
-    const int sizes=j;
-    int arr[sizes];
-    srand(time(NULL));
-    for(int i=0;i<sizes;i++){
-        arr[i]=rand()%100;
-
-    }
-
-
-     auto t_start = chrono::high_resolution_clock::now();
-
-     quicksort(arr,0,sizes-1);
-
-    auto t_end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(t_end - t_start).count();
-    cout << "Size of array =\t" << j <<"\tTime taken = "<<duration<<endl;
+int main(){   
+    int sizze;
+    
+    for(int j=1000;j<=10000;j+=2000)
+    {
+        const int sizes=j;
+        int arr[sizes];
+        srand(time(NULL));
+        for(int i=0;i<sizes;i++)
+        {
+            arr[i]=rand()%100;
+    
+        }
+    
+    
+        auto t_start = chrono::high_resolution_clock::now();
+    
+        quicksort(arr,0,sizes-1);
+    
+        auto t_end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(t_end - t_start).count();
+        cout << "Size of array =\t" << j <<"\tTime taken = "<<duration<<endl;
 
 }
 
